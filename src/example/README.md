9eb09df53f421061d48145e0f41ddfe9dcfb0d36
chunleili <li_cl@foxmail.com>
2022-12-16 21:17:23 +0800

    bug fix

---
+++ src/example/README.md
@@ -0,0 +1,12 @@
+# 例子
+每个例子均可单独运行
+
+## SPH
+SPH采用erizmr的SPH_Taichi为主体，进行修改。
+
+运行src\algorithm\SPH\SPH_Taichi\run_simulation.py以查看下面的例子：
+![SPH_example](/src/algorithm/SPH/SPH_Taichi/data/gif/no_gravity_collision.gif)
+
+## LBM
+运行src\example\test_lbm.py 以进行LBM计算
+(FIXME: dead loop bug)
--- src/example/test_lbm.py
+++ src/example/test_lbm.py
@@ -1,6 +1,10 @@
+import os, sys
+cwd = os.path.abspath(os.getcwd())
+cwd+=r"\src"
+sys.path.append(cwd)
+
 import taichi as ti
 ti.init(arch=ti.cuda)
-
 from algorithm.LBM.LBM import LbmSolver
 
 solver = LbmSolver(tau=0.57, res=(64, 64, 64), gravity=(0.0, 0.0, -0.0007), smag_constant=0.04)
